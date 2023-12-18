c9ab1eba28dc50faf724093f4ef3a9898aaca7cc
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2023-12-18 12:26:53 +0100

    dockefile turtlebot deps

--- .docker/build_image.sh
+++ .docker/build_image.sh
@@ -5,5 +5,4 @@ echo -e "Building turtlebot_robot:lastest image"
 DOCKER_BUILDKIT=1 \
 docker build --pull --rm -f ./.docker/Dockerfile \
 --build-arg BUILDKIT_INLINE_CACHE=1 \
---target bash \
 --tag turtlebot_robot:latest .
---
+++ README.md
@@ -0,0 +1,4 @@
+# turtlebot robot
+
+[rplidar udev rules](https://github.com/Slamtec/rplidar_ros/blob/master/debian/udev)
+
