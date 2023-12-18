00efc453e725881b0548bc0e71cbc2a6f4c2eaf5
huanyu <HuanyuL@users.noreply.github.com>
2023-12-18 18:02:30 +0100

    change laser to baselink

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 source "/opt/ros/noetic/setup.bash"
 source "/dev_ws/devel/setup.bash"
 
+export ROS_MASTER_URI=http://10.41.1.1:11311
+export ROS_IP=10.41.1.1
 exec "$@"
--- .docker/setup.bash
+++ .docker/setup.bash
@@ -1,2 +1,5 @@
 source "/opt/ros/noetic/setup.bash"
 source "/dev_ws/devel/setup.bash"
+
+export ROS_MASTER_URI=http://10.41.1.1:11311
+export ROS_IP=10.41.1.1
